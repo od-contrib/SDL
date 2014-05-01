cfe3214ea0fb84d3ef404d350951d7b48fd6873c
Paul Cercueil <paul@crapouillou.net>
2014-05-01 18:51:45 +0200

    Added first implementation of the FBDEV video driver

--- src/video/SDL_sysvideo.h
+++ src/video/SDL_sysvideo.h
@@ -419,6 +419,10 @@ extern VideoBootStrap VIVANTE_bootstrap;
 extern VideoBootStrap Emscripten_bootstrap;
 extern VideoBootStrap QNX_bootstrap;
 
+#if SDL_VIDEO_DRIVER_FBDEV
+extern VideoBootStrap FBDEV_bootstrap;
+#endif
+
 extern SDL_VideoDevice *SDL_GetVideoDevice(void);
 extern int SDL_AddBasicVideoDisplay(const SDL_DisplayMode * desktop_mode);
 extern int SDL_AddVideoDisplay(const SDL_VideoDisplay * display);

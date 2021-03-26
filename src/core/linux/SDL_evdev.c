begui/SDL2

evdev: Don't warn on unknown scancode
In our case, this warning appears all the time but the keys are still
perfectly recognized.

src/core/linux/SDL_evdev.c
@@ -711,15 +711,10 @@ SDL_EVDEV_Poll(void)
 static SDL_Scancode
 SDL_EVDEV_translate_keycode(int keycode)
 {
-    SDL_Scancode scancode = SDL_SCANCODE_UNKNOWN;
-
-    if (keycode < SDL_arraysize(EVDEV_Keycodes)) {
-        scancode = EVDEV_Keycodes[keycode];
-    }
-    if (scancode == SDL_SCANCODE_UNKNOWN) {
-        SDL_Log("The key you just pressed is not recognized by SDL. To help get this fixed, please report this to the SDL mailing list <sdl@libsdl.org> EVDEV KeyCode %d \n", keycode);
-    }
-    return scancode;
+    if (keycode < SDL_arraysize(EVDEV_Keycodes))
+        return EVDEV_Keycodes[keycode];
+    else
+        return SDL_SCANCODE_UNKNOWN;
 }
 
 static void

od-contrib/SDL

src/core/linux/SDL_evdev.c
@@ -446,21 +446,8 @@ SDL_EVDEV_translate_keycode(int keycode)
 {
     SDL_Scancode scancode = SDL_SCANCODE_UNKNOWN;
 
-    if (keycode < SDL_arraysize(linux_scancode_table)) {
+    if (keycode < SDL_arraysize(linux_scancode_table))
         scancode = linux_scancode_table[keycode];
-
-        if (scancode == SDL_SCANCODE_UNKNOWN) {
-            /* BTN_TOUCH is handled elsewhere, but we might still end up here if
-               you get an unexpected BTN_TOUCH from something SDL believes is not
-               a touch device. In this case, we'd rather not get a misleading
-               SDL_Log message about an unknown key. */
-            if (keycode != BTN_TOUCH) {
-                SDL_Log("The key you just pressed is not recognized by SDL. To help "
-                    "get this fixed, please report this to the SDL forums/mailing list "
-                    "<https://discourse.libsdl.org/> EVDEV KeyCode %d", keycode);
-            }
-        }
-    }
 
     return scancode;
 }